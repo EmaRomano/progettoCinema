40b7d83f78ee731cc3aa85a4588a9ad24d3cc438
Emanuele Romano <74961029+EmRom666@users.noreply.github.com>
2021-07-04 14:14:47 +0200

    raffinato il metodo creaSfondoScalatoSu: eliminato l'overloading e uso della riflessione

--- progetto cinema/src/gui/SuperJFrame.java
+++ progetto cinema/src/gui/SuperJFrame.java
@@ -4,9 +4,12 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Image;
+import java.lang.reflect.InvocationTargetException;
 
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -18,10 +21,10 @@ public class SuperJFrame extends JFrame {
 
 	private JPanel contentPane;
 	private ControllerGUI controllerGUI;
-	
+
 	public SuperJFrame(ControllerGUI controllerGUI) {
 		this.controllerGUI=controllerGUI;
-		
+
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
@@ -29,21 +32,16 @@ public class SuperJFrame extends JFrame {
 		ImageIcon iconaSigma = new ImageIcon(getClass().getResource("/logoSigma.png"));
 		setIconImage(iconaSigma.getImage());
 	}
-	
-	public void creaSfondoScalatoSu(JButton componente, String nomeFileImmagine ) {
-		ImageIcon immagineIcona = new ImageIcon(getClass().getResource("/" + nomeFileImmagine));
-		Image immagine=(immagineIcona).getImage().getScaledInstance(componente.getWidth(),
-			componente.getHeight(), Image.SCALE_SMOOTH);
-		immagineIcona = new ImageIcon(immagine);
-		componente.setIcon(immagineIcona);	
-	}	
-	
-	public void creaSfondoScalatoSu(JLabel componente, String nomeFileImmagine ) {
-		ImageIcon immagineIcona = new ImageIcon(getClass().getResource("/" + nomeFileImmagine));
-		Image immagine=(immagineIcona).getImage().getScaledInstance(componente.getWidth(),
-			componente.getHeight(), Image.SCALE_SMOOTH);
-		immagineIcona = new ImageIcon(immagine);
-		componente.setIcon(immagineIcona);	
+
+	public void creaSfondoScalatoSu(JComponent componente, String nomeFileImmagine) {
+		Image immagine = new ImageIcon(getClass().getResource("/" + nomeFileImmagine)).getImage();
+		ImageIcon immagineIcona = new ImageIcon(immagine.getScaledInstance(componente.getWidth(),componente.getHeight(), Image.SCALE_SMOOTH));
+		Class<? extends JComponent> c = componente.getClass();
+		try{
+			c.getMethod("setIcon", Icon.class).invoke(componente, immagineIcona);
+		}catch(NoSuchMethodException | IllegalAccessException | SecurityException | IllegalArgumentException | InvocationTargetException e){
+			e.printStackTrace();
+		}
 	}
 
 }
