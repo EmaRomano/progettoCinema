1eb2d2c3b13421820b8617c4ca86f0bb16487992
Emanuele Romano <74961029+EmRom666@users.noreply.github.com>
2021-09-08 15:24:13 +0200

    prova commit tramite github desktop

--- progetto cinema/src/Driver.java
+++ progetto cinema/src/Driver.java
@@ -13,15 +13,6 @@ public class Driver {
 	}
 	
 	//TODO ultimo salvataggio in copia4 l' 8 settembre
-	// praticamente finito il progetto
-	// ultime cose fatte:
-	//eliminazione inserisciSpettacolo e relative JD
-	//uso di NotificaJD
-	//eliminazione di molteJD
-	//eliminazione di IntegerSpinner
-	//aggiustati gli attributi di EditaSpettacoloJF
-	//tolta associazione AvvioJF con ControllerGUI
-	//aggiustata CancellaSpettacoloJF (tolti tutti gli attributi)
-	//eliminata associazione di spettacoloGUI con EditaSpettacoloJF
+    // prova commit tramite github desktop
 
 }
